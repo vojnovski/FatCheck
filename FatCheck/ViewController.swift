36965f7b0cdd5c16d76ebfbb60e94f588371455f
Viktor Vojnovski <vojnovski@gmail.com>
2015-10-04 01:32:59 +0200

    Initial Commit

---
+++ FatCheck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FatCheck
+//
+//  Created by Viktor Vojnovski on 04.10.15.
+//  Copyright © 2015 Viktor Vojnovski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FatCheckTests/FatCheckTests.swift
@@ -0,0 +1,36 @@
+//
+//  FatCheckTests.swift
+//  FatCheckTests
+//
+//  Created by Viktor Vojnovski on 04.10.15.
+//  Copyright © 2015 Viktor Vojnovski. All rights reserved.
+//
+
+import XCTest
+@testable import FatCheck
+
+class FatCheckTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
